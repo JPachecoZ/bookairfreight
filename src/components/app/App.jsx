2d2ba059d27651619f460618b73cb6d0ae4f73c6
JPachecoZ <jepach@gmail.com>
2023-01-31 15:10:21 -0500

    fix: position of error messages

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -35,30 +35,38 @@ export function App() {
   return (
     <div className="app-container">
       <form onSubmit={handleSubmit} className="form-container" noValidate>
-        <label className="app-label">
-          Starting country
-          <input type="text" name="startingCountry" onChange={handleChange} value={preQuote.startingCountry}/>
-        </label>
-        <p className="error-message">{quoteErrors.startingCountry}</p>
-        <label className="app-label">
-          Destination country
-          <input type="text" name="destinationCountry" onChange={handleChange} value={preQuote.destinationCountry}/>
-        </label>
-        <p className="error-message">{quoteErrors.destinationCountry}</p>
-        <label className="app-label">
-          Quote price
-          <input type="number" name="quotePrice" onChange={handleChange} value={preQuote.quotePrice}/>
-        </label>
-        <p className="error-message">{quoteErrors.quotePrice}</p>
-        <label className="app-label">
-          Shipping Channel
-          <select name="shippingChannel" onChange={handleChange} value={preQuote.shippingChannel}>
-            {shippingChannels.map((channel) =>
-            <option key={channel} >{channel}</option>
-            )}
-          </select>
-        </label>
+        <div className="input-container">
+          <label className="app-label">
+            Starting country
+            <input type="text" name="startingCountry" onChange={handleChange} value={preQuote.startingCountry}/>
+          </label>
+          <p className="error-message">{quoteErrors.startingCountry}</p>
+        </div>
+        <div className="input-container">
+          <label className="app-label">
+            Destination country
+            <input type="text" name="destinationCountry" onChange={handleChange} value={preQuote.destinationCountry}/>
+          </label>
+          <p className="error-message">{quoteErrors.destinationCountry}</p>
+        </div>
+        <div className="input-container">
+          <label className="app-label">
+            Quote price
+            <input type="number" name="quotePrice" onChange={handleChange} value={preQuote.quotePrice}/>
+          </label>
+          <p className="error-message">{quoteErrors.quotePrice}</p>
+        </div>
+        <div className="input-container">
+          <label className="app-label">
+            Shipping Channel
+            <select name="shippingChannel" onChange={handleChange} value={preQuote.shippingChannel}>
+              {shippingChannels.map((channel) =>
+              <option key={channel} >{channel}</option>
+              )}
+            </select>
+          </label>
         <p className="error-message">{quoteErrors.shippingChannel}</p>
+        </div>
         <button className="app-button" type="submit">Create quote</button>
       </form>
       {quote && <Quote quote={quote}/>}
